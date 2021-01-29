0bd027db72fad9d8d27d28209dd4259bf8a45ccd
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-01-29 12:27:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import os
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config
+    from config import config
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
