66661928e23fe5ae496cabfaa28da1154059d98d
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-01-29 10:38:26 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,13 +1,22 @@
 import os
 
-from telethon.tl.types import ChatBannedRights
-
 class Config(object):
 
 
     LOGGER = True
     # Get your own API key from https://www.remove.bg/ or
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    # The Telegram API things
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
+    # Get these values from my.telegram.org
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    # Banned Unwanted Members..
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+    # the download location, where the HTTP Server runs
 
 
 class Production(Config):
