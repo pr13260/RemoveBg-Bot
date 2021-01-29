4b423953104775f4ecdf29bc6128b4bafb5dd505
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-01-29 10:31:58 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -8,3 +8,11 @@ class Config(object):
     LOGGER = True
     # Get your own API key from https://www.remove.bg/ or
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
