61d4c0a26b11f66c8b5a3ce470bc3e86788b38ed
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-01-30 15:49:00 +0530

    Update removebg.py

--- plugins/removebg.py
+++ plugins/removebg.py
@@ -18,15 +18,15 @@ async def remove_background(event):
     message_id = await reply_id(event)
     if event.reply_to_msg_id and not input_str:
         reply_message = await event.get_reply_message()
-        catevent = await edit_or_reply(event, "`Analysing this Image/Sticker...`")
+        botevent = await edit_or_reply(event, "`Analysing this Image/Sticker...`")
         file_name = os.path.join(Config.TEMP_DIR, "rmbg.png")
         try:
             await event.client.download_media(reply_message, file_name)
         except Exception as e:
-            await edit_delete(catevent, f"`{str(e)}`", 5)
+            await edit_delete(botevent, f"`{str(e)}`", 5)
             return
         else:
-            await catevent.edit("`Removing Background of this media`")
+            await botevent.edit("`Removing Background of this media`")
             file_name = convert_toimage(file_name)
             response = ReTrieveFile(file_name)
             os.remove(file_name)
@@ -46,7 +46,7 @@ async def remove_background(event):
         with open("backgroundless.png", "wb") as removed_bg_file:
             removed_bg_file.write(response.content)
     else:
-        await edit_delete(catevent, f"`{response.content.decode('UTF-8')}`", 5)
+        await edit_delete(botevent, f"`{response.content.decode('UTF-8')}`", 5)
         return
     if cmd == "srmbg":
         file = convert_tosticker(remove_bg_image, filename="backgroundless.webp")
@@ -63,7 +63,7 @@ async def remove_background(event):
             force_document=True,
             reply_to=message_id,
         )
-    await catevent.delete()
+    await botevent.delete()
 
 
 # this method will call the API, and return in the appropriate format
